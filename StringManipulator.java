5829ba7f231f1dd52b2e4bee6112d08914d0883c
Javier Rivera <Sunnuke@users.noreply.github.com>
2020-11-27 14:05:44 -0800

    Created StringManipulator.java/class and Added a layout of the methods being used to manipulate strings

---
+++ StringManipulator.java
@@ -0,0 +1,14 @@
+public class StringManipulator {
+    public String trimAndConcat(String str1, String str2) {
+        
+    }
+    public Integer getIndexOrNull(String str1, char chr1) {
+        
+    }
+    public Integer getIndexOrNull(String str1, String str2) {
+        
+    }
+    public String concatSubstring(String str1, int num1, int num2, String str2) {
+        
+    }
+}
